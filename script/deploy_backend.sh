015faaf6e7d071e94959a064242e58846641e0d8
Ubuntu <ubuntu@ip-172-31-95-143.ec2.internal>
2023-11-15 13:30:06 +0000

    Creación de la base de datos

---
+++ script/deploy_backend.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Muestra todos los comandos que se han ejeutado.
+
+set -ex
+
+# Actualización de repositorios
+ sudo apt update
+
+# Actualización de paquetes
+# sudo apt upgrade  
+
+# Incluimos las variables del archivo .env.
+source .env
+
+# Creamos la base de datos.
+mysql -u root <<< "DROP DATABASE IF EXISTS $WORDPRESS_DB_NAME"
+mysql -u root <<< "CREATE DATABASE $WORDPRESS_DB_NAME"
+mysql -u root <<< "DROP USER IF EXISTS $WORDPRESS_DB_USER@$IP_CLIENTE_MYSQL"
+mysql -u root <<< "CREATE USER $WORDPRESS_DB_USER@$IP_CLIENTE_MYSQL IDENTIFIED BY '$WORDPRESS_DB_PASSWORD'"
+mysql -u root <<< "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO $WORDPRESS_DB_USER@$IP_CLIENTE_MYSQL"
+
+
+
+
