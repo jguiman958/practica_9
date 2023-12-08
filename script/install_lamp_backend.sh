bd5aeb795442117f1f869ff7533e88fe82f69a4b
Ubuntu <ubuntu@ip-172-31-81-116.ec2.internal>
2023-12-08 10:02:27 +0000

    ---

--- script/install_lamp_backend.sh
+++ script/install_lamp_backend.sh
@@ -5,7 +5,7 @@
 set -ex
 
 # Actualización de repositorios
- sudo apt update
+apt update
 
 # Actualización de paquetes
 # sudo apt upgrade  
@@ -14,7 +14,7 @@ set -ex
 source .env
 
 #instalar mysql server
-sudo apt install mysql-server -y
+apt install mysql-server -y
 
 # Configuración de mysql, para que solo acepte conexiones desde la ip privada.
 sed -i "s/127.0.0.1/$MYSQL_PRIVATE/" /etc/mysql/mysql.conf.d/mysqld.cnf
